3d5dd9ab1f4f81e49969fc176182e6e5fb59122e
Franca Mgbogu <franciedigital@gmail.com>
2024-02-10 14:35:09 +0100

    Add functionality to print current page using window.print() method

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 document.getElementById("changeTextBtn").addEventListener("click", function() {
     document.getElementById("demo").innerHTML = "Hello JavaScript!";
 });
+
+
+document.getElementById("changeTextBtn1").addEventListener("click", function(){
+    document.getElementById("demo1").style.fontSize='35px';
+});
+
+document.getElementById("hideTextBtn").addEventListener("click",function(){
+    document.getElementById("demo2").style.display="none";
+});
+
+document.getElementById("showTextBtn").addEventListener("click", function(){
+    document.getElementById("demo3").style.display="block";
+});
+
+document.getElementById("newdemo").innerHTML = 5 + 6;
+
+document.getElementById("windowpm").addEventListener("click", function(){
+    window.print();
+});
