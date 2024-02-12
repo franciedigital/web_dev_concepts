e26c178450025ee7d16197e6a638d1df189ce3b9
Franca Mgbogu <franciedigital@gmail.com>
2024-02-12 11:41:34 +0100

    Ensure the button with ID "jsstate" is displayed correctly by reviewing HTML and JavaScript code.

--- main.js
+++ main.js
@@ -20,3 +20,19 @@ document.getElementById("newdemo").innerHTML = 5 + 6;
 document.getElementById("windowpm").addEventListener("click", function(){
     window.print();
 });
+
+
+let x, y, z; //statement 1
+x = 5;       // Statement 2
+y = 6;       //  Statement 3
+z = x + y;   // statement 4
+
+document.getElementById("newdemo1").innerHTML = "The value of z is " + z +".";
+
+document.getElementById("democoma").innerHTML =
+"Hello Dolly!";
+
+document.getElementById("jsstate").addEventListener("click", function(){
+    document.getElementById("newdemo2").innerHTML = "Hello Dolly!";
+    document.getElementById("newdemo3").innerHTML = "How are you?";
+})
